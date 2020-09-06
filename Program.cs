fb4f2c13bf7bd684f6ab4bfed84770882aac507d
Richard Thomas <rht341@gmail.com>
2020-09-06 12:04:45 -0400

    Sort of working.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using unirest_net.http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ChessStats
 {
     class Program
     {
+        private const string Url = "https://api.chess.com/pub/player/rht609/stats";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chess Stats!");
+            //Console.WriteLine("Hello Chess Stats!");
+
+            HttpResponse<string> response = Unirest.get(Url)
+                                                   .header("Accept", "application/json")
+                                                   .asJson<string>();
+
+            //Console.WriteLine(response.Body.ToString());
+            
+            dynamic stats = JObject.Parse(response.Body.ToString());
+
+            string blitzRating = stats.chess_blitz.last.rating;
+            Console.WriteLine(blitzRating);
         }
     }
 }
